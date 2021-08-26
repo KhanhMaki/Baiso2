0ab2820cd9795a4bab292946ff11f4e078363d6d
Administrator <Administrator@DESKTOP-8F6DJTS>
2021-08-26 22:50:14 +0700

    xong bt 2

---
+++ Baiso2/Baiso2/Controllers/BaiTap2Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Baiso2.Models;
+
+namespace Baiso2.Controllers
+{
+    public class BaiTap2Controller : Controller
+    {
+        // GET: BaiTap2
+        giaibaitap2 gpt = new giaibaitap2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BT2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult BT2(double hsa, double hsb)
+        {
+            double x = gpt.giaiphuongtrinhbac1(hsa, hsb);
+            ViewBag.nghiem = x;
+            return View();
+        }
+    }
+}
---
+++ Baiso2/Baiso2/Models/giaibaitap2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Baiso2.Models
+{
+    public class giaibaitap2
+    {
+        public double giaiphuongtrinhbac1(double a , double b)
+        {
+            double x = -b / a;
+            return x;
+        }
+    }
+}
